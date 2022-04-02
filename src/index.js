66eb0dc3670cbc152c37384b26ee56fb3bb8cff8
JulioA <ti@electrodomotica.com.ve>
2022-04-01 21:23:29 -0400

    Rechequeando el puerto 3000

--- src/index.js
+++ src/index.js
@@ -32,7 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.listen(3000, () => {
 // app.listen(process.env.PORT || 4000, () => {
     app.listen(3000, () => {
-    console.log('Server running on port:', process.env.PORT);
+    // console.log('Server running on port:', process.env.PORT);
+    console.log('Server running on port:', 3000);
 });
 
 
